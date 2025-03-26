f56bf3545e07ad675cfcc7f0179db8eaca745865
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2025-03-26 16:47:28 +0100

    set up repro

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,18 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { ssgPlugin } from '@modern-js/plugin-ssg';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   runtime: {
     router: true,
   },
+  output: {
+    ssg: true,
+  },
   plugins: [
     appTools({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
     }),
+    ssgPlugin(),
   ],
 });
