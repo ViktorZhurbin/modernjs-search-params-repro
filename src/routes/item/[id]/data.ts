d27cf4c95c31a8c356f4814fe39925f8eab74257
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2025-04-01 17:18:32 +0200

    test env variable

--- src/routes/item/[id]/data.ts
+++ src/routes/item/[id]/data.ts
@@ -3,6 +3,11 @@ import type { LoaderFunctionArgs } from '@modern-js/runtime/router';
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`,
+    {
+      headers: {
+        Authorization: `Token ${process?.env?.TOKEN}`,
+      },
+    },
   );
   const data = await response.json();
 
